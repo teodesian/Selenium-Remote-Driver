9b8362c96bada050581fc4f89377dc5c20bd073b
Gordon Child <gordon@gordonchild.com>
2011-08-01 23:57:56 -0500

    updated driver-example.pl on how to use Selenium::Remote::Driver with Test::More

--- driver-example.pl
+++ driver-example.pl
@@ -1,12 +1,14 @@
 #!/usr/bin/perl
 use Selenium::Remote::Driver;
+use Test::More tests=>4;
 
 my $driver = Selenium::Remote::Driver->new;
 $driver->get("http://www.google.com");
-my $element = $driver->find_element('q','name');
-$element->send_keys("Hello WebDriver!");
-$element->submit;
+$driver->find_element('q','name')->send_keys("Hello WebDriver!");
 
-print $driver->get_title() . "\n";
+ok($driver->get_title =~ /Google/,"title matches google");
+is($driver->get_title,'Google',"Title is google");
+ok($driver->get_title eq 'Google','Title equals google');
+like($driver->get_title,qr/Google/,"Title matches google");
 
 $driver->quit();
