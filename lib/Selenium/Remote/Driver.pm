6e7bdf5001733d4b88ea0296d06819cb5e1d8f8f
Daniel Gempesaw <gempesaw@gmail.com>
2013-10-25 00:44:36 -0400

    Use the more concise logical defined-or for auto_close determination
    Logical defined-or is more concise, easier to read, and visually
    consistent with the rest of the initialization options, than the ternary
    statement I was previously using.
    
    http://perldoc.perl.org/perlop.html#Logical-Defined-Or

--- lib/Selenium/Remote/Driver.pm
+++ lib/Selenium/Remote/Driver.pm
@@ -178,7 +178,7 @@ sub new {
         session_id         => undef,
         remote_conn        => undef,
         commands           => $ress,
-        auto_close         => defined $args{auto_close} ? delete $args{auto_close} : 1,
+        auto_close         => delete $args{auto_close} // 1,
         pid                => $$,
     };
     bless $self, $class or die "Can't bless $class: $!";
