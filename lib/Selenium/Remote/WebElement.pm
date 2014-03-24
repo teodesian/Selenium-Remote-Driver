2752642586dae4674a960101f35682885838474d
gempesaw <gempesaw@gmail.com>
2014-03-24 11:12:37 -0400

    Handle AUTHOR and COPYRIGHT pod in weaver.ini

--- lib/Selenium/Remote/WebElement.pm
+++ lib/Selenium/Remote/WebElement.pm
@@ -458,25 +458,3 @@ L<http://github.com/gempesaw/Selenium-Remote-Driver/issues>.
 =head1 CURRENT MAINTAINER
 
 Daniel Gempesaw C<< <gempesaw@gmail.com> >>
-
-=head1 AUTHOR
-
-Perl Bindings for Selenium Remote Driver by Aditya Ivaturi C<< <ivaturi@gmail.com> >>
-
-=head1 LICENSE
-
-Copyright (c) 2010-2011 Aditya Ivaturi, Gordon Child
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-480:    hit eof while in pod documentation (no =cut seen)
-        this can cause trouble with some pod utilities
