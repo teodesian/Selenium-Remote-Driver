444e21b61eb8d3487b289e274ec84eb9ac6c2d6a
Daniel Gempesaw <gempesaw@gmail.com>
2014-03-27 08:11:41 -0400

    Teach test runner about windows

--- t/bin/generate-recordings.pl
+++ t/bin/generate-recordings.pl
@@ -13,8 +13,11 @@ if ($^O eq 'linux') {
     print "Headless and need a webdriver server started? Try\n\n\tDISPLAY=:1 xvfb-run --auto-servernum java -jar /usr/lib/node_modules/protractor/selenium/selenium-server-standalone-2.40.0.jar\n\n";
 }
 
+my $export = $^O eq 'MSWin32' ? 'set' : 'export';
+my $srdLib = glob('Selenium-Remote-Driver*/lib');
+
 print `dzil build`;
-print `export WD_MOCKING_RECORD=1 && perl -I"Selenium-Remote-Driver/lib" t/01-driver.t && perl -I"Selenium-Remote-Driver/lib" t/02-webelement.t`;
+print `$export WD_MOCKING_RECORD=1 && perl -I$srdLib t/01-driver.t && perl -I$srdLib t/02-webelement.t`;
 killServer();
 
 sub startServer {
