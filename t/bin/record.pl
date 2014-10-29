48f1c63b1f1aff4aee93d3f07822584b1119d5a8
Daniel Gempesaw <gempesaw@gmail.com>
2014-10-28 22:34:13 -0400

    Cleanup automatic recording script
    - renamed `$srd_folder` to `$repo_root` for clarity
    - use `prove` instead of manually listing out files

--- t/bin/record.pl
+++ t/bin/record.pl
@@ -7,45 +7,31 @@ use Cwd qw/abs_path/;
 use FindBin;
 # We can only dzil from the root of the repository.
 my $this_folder = $FindBin::Bin . '/../../'; # t/bin/../../
-my $srd_folder = abs_path($this_folder) . '/';
+my $repo_root = abs_path($this_folder) . '/';
 
 reset_env();
 start_server();
 
 print 'Cleaning...and building...
 ';
-print `cd $srd_folder && dzil build`;
+print `cd $repo_root && dzil build`;
 
 if ($^O eq 'linux') {
     print "Headless and need a webdriver server started? Try\n\n\tDISPLAY=:1 xvfb-run --auto-servernum java -jar /usr/lib/node_modules/protractor/selenium/selenium-server-standalone-2.42.2.jar\n\n";
 }
 
-my @files = map { $srd_folder . $_ } (
-    't/01-driver.t',
-    't/02-webelement.t',
-    't/Firefox-Profile.t'
-);
-
-my $srd_lib = glob($srd_folder . 'Selenium-Remote-Driver*/lib');
-my $t_lib = glob($srd_folder . 'Selenium-Remote-Driver*');
-
-my $execute_tests = join( ' && ', map {
-    'perl -I' . $srd_lib
-      . ' -I' . $t_lib
-      . ' ' . $_
-  } @files);
-
+my $built_lib = $repo_root . glob('Selenium-Remote-Driver-*/lib');
 my $export = $^O eq 'MSWin32' ? 'set' : 'export';
 my $wait = $^O eq 'MSWin32' ? 'START /WAIT' : '';
-print `$export WD_MOCKING_RECORD=1 && $wait $execute_tests`;
+print `$export WD_MOCKING_RECORD=1 && cd $repo_root && prove -I$built_lib -rv t/`;
 reset_env();
 
 sub start_server {
     if ($^O eq 'MSWin32') {
-        system('start "TEMP_HTTP_SERVER" /MIN perl ' . $srd_folder . 't/http-server.pl');
+        system('start "TEMP_HTTP_SERVER" /MIN perl ' . $repo_root . 't/http-server.pl');
     }
     else {
-        system('perl ' . $srd_folder . 't/http-server.pl > /dev/null &');
+        system('perl ' . $repo_root . 't/http-server.pl > /dev/null &');
     }
 }
 
@@ -60,6 +46,6 @@ sub kill_server {
 
 
 sub reset_env {
-    `cd $srd_folder && dzil clean`;
+    `cd $repo_root && dzil clean`;
     kill_server();
 }
